b608d97d60af9c49c3e3b615dc345faf47db4298
yellowbricks <135861147+yellowbrickssss@users.noreply.github.com>
2026-02-20 13:53:48 +0900

    마도대전 음악 수정, 빛요한 추가

--- data.js
+++ data.js
@@ -734,7 +734,7 @@ const HERO_DATA = [
             {
                 id: "v_auto_1771496164",
                 label: "영웅 화면 33",
-                transcript: "꼬맹아, 왜? 뭐가 좋다고 자꾸 힐끔힐끔 쳐다보냐? 뭐? 나이가 비슷한 줄 알았다고? 응, 그럴 수도 있지. 실수 한 번 정도는 용서해 줄 수 있어. 다음부터는 그러지 마라. 통영니임, 하고 부르는 거야. 알겠어?",
+                transcript: "꼬맹아, 왜? 뭐가 좋다고 자꾸 힐끔힐끔 쳐다보냐? 뭐? 나이가 비슷한 줄 알았다고? 응, 그럴 수도 있지. 실수 한 번 정도는 용서해 줄 수 있어. 다음부터는 그러지 마라. 통령니임, 하고 부르는 거야. 알겠어?",
                 audio: "rosanna/water_rosanna/물-로잔나 영웅창 (32).mp3"
             },
             {
@@ -995,6 +995,265 @@ const HERO_DATA = [
         title: "광명의 기사",
         image: "johan/light_johan/light_johan.png",
         voices: [
+                        {
+                id: "v_auto_1771541912",
+                label: "자기소개",
+                transcript: "이전에도, 이후에도, 저의 군주는 로드 뿐입니다.",
+                audio: "johan/light_johan/빛요한 자기소개.mp3"
+            },
+            {
+                id: "v_auto_1771542309",
+                label: "영웅 영입 1",
+                transcript: "이 몸과 마음은 지금부터 로드의 것입니다.",
+                audio: "johan/light_johan/빛요한 영웅 영입 1.mp3"
+            },
+            {
+                id: "v_auto_1771542343",
+                label: "영웅 영입 2",
+                transcript: "로드의 검으로서, 부끄럽지 않은 기사가 되겠습니다.",
+                audio: "johan/light_johan/빛요한 영웅 영입 2.mp3"
+            },
+            {
+                id: "v_auto_1771541979",
+                label: "영웅 초월 1",
+                transcript: "제 힘이, 로드의 힘이 될 것입니다.",
+                audio: "johan/light_johan/빛요한 영웅 초월 1.mp3"
+            },
+            {
+                id: "v_auto_1771542044",
+                label: "영웅 초월 2",
+                transcript: "성장의 기쁨은 무엇과도 비교할 수 없죠.",
+                audio: "johan/light_johan/빛요한 영웅 초월 2.mp3"
+            },
+            {
+                id: "v_auto_1771542076",
+                label: "영웅 초월 3",
+                transcript: "더 정진하겠습니다. 로드.",
+                audio: "johan/light_johan/빛요한 영웅 초월 3.mp3"
+            },
+            {
+                id: "v_auto_1771542111",
+                label: "영웅 초월 4",
+                transcript: "과거의 자신을 넘어선 기분입니다.",
+                audio: "johan/light_johan/빛요한 영웅 초월 4.mp3"
+            },
+            {
+                id: "v_auto_1771542165",
+                label: "영웅 각성 1",
+                transcript: "로드께 힘이 될 수 있어 기쁩니다!",
+                audio: "johan/light_johan/빛요한 영웅 각성 1.mp3"
+            },
+            {
+                id: "v_auto_1771542197",
+                label: "영웅 각성 2",
+                transcript: "노력은 배신하지 않는 법이죠.",
+                audio: "johan/light_johan/빛요한 영웅 각성 2.mp3"
+            },
+            {
+                id: "v_auto_1771542252",
+                label: "영웅 각성 3",
+                transcript: "로드의 검인 저에게, 한계는 없습니다.",
+                audio: "johan/light_johan/빛요한 영웅 각성 3.mp3"
+            },
+            {
+                id: "v_auto_1771543765",
+                label: "타이틀 콜",
+                transcript: "Lord Of Heroes.",
+                audio: "johan/light_johan/빛요한 타이틀 콜.mp3"
+            },
+            {
+                id: "v_auto_1771544570",
+                label: "영웅 화면 1",
+                transcript: "병사들의 훈련 상태는 매우 양호합니다. 제가 직접 지도하고 있으니까요.",
+                audio: "johan/light_johan/빛요한 영웅 화면 1.mp3"
+            },
+            {
+                id: "v_auto_1771544722",
+                label: "영웅 화면 2",
+                transcript: "아이들이야말로 아발론의 미래겠죠. 그들의 웃음을 지킬 수 있다면 무엇이든 할 자신이 있습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 2.mp3"
+            },
+            {
+                id: "v_auto_1771544743",
+                label: "영웅 화면 3",
+                transcript: "저는 로드께서 모두가 행복한 세상을 만들어 주시리라 믿습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 3.mp3"
+            },
+            {
+                id: "v_auto_1771544801",
+                label: "영웅 화면 4",
+                transcript: "프람 님이 결코 나쁜 분이 아니시라는 건 알지만, 그래도 대련 상대에게 좀 자비를 베풀어 주셨으면 좋겠는데요. 어제도….",
+                audio: "johan/light_johan/빛요한 영웅 화면 4.mp3"
+            },
+            {
+                id: "v_auto_1771544872",
+                label: "영웅 화면 5",
+                transcript: "루인 님이 계셔서 참 다행입니다. 저는 군사학에만 조금 흥미가 있을 뿐이니, 내정을 총괄해 주시는 분이 있다는 게 얼마나 든든한지 모릅니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 5.mp3"
+            },
+            {
+                id: "v_auto_1771544919",
+                label: "영웅 화면 6",
+                transcript: "세상의 모든 악을 심판하는 날이 오리라 믿습니다. 정의는, 이루어질 것입니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 6.mp3"
+            },
+            {
+                id: "v_auto_1771544996",
+                label: "영웅 화면 7",
+                transcript: "악인들을 엄정히 처벌할수록, 세상은, 더 행복해질 것입니다. 반드시…. 그렇게 되겠죠.",
+                audio: "johan/light_johan/빛요한 영웅 화면 7.mp3"
+            },
+            {
+                id: "v_auto_1771545050",
+                label: "영웅 화면 8",
+                transcript: "안경, 말입니까? 아, 조금 더 인상을 부드럽게 만들기 위해 써 봤는데…. 이상한가요?",
+                audio: "johan/light_johan/빛요한 영웅 화면 8.mp3"
+            },
+            {
+                id: "v_auto_1771545114",
+                label: "영웅 화면 9",
+                transcript: "재미 없다는 평가는 익숙합니다. 아하하, 저는 그저 성실한 모습을 보여 드리겠습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 9.mp3"
+            },
+            {
+                id: "v_auto_1771545148",
+                label: "영웅 화면 10",
+                transcript: "힘든 순간은 갑자기 닥쳐옵니다. 그것을 대비하기란 정말 어려운 일이죠.",
+                audio: "johan/light_johan/빛요한 영웅 화면 10.mp3"
+            },
+                        {
+                id: "v_auto_1771556092",
+                label: "영웅 화면 11",
+                transcript: "오셨습니까, 로드. 병사들을 훈련시키고 있었습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 11.mp3"
+            },
+            {
+                id: "v_auto_1771556118",
+                label: "영웅 화면 12",
+                transcript: "저 요한은, 로드께서 돌아오시리라 굳게 믿고 있었습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 12.mp3"
+            },
+            {
+                id: "v_auto_1771556168",
+                label: "영웅 화면 13",
+                transcript: "샬롯도 많이 밝아졌죠. 예전의 낯 가리던 모습은 찾아볼 수 없네요. 잘 자라 주어서 고마울 따름입니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 13.mp3"
+            },
+            {
+                id: "v_auto_1771556240",
+                label: "영웅 화면 14",
+                transcript: "아, 미하일 경이 추천해 주신 곳은 항상 훌륭합니다. 좋은 취미를 가진 동료가 있어서 든든하네요.",
+                audio: "johan/light_johan/빛요한 영웅 화면 14.mp3"
+            },
+            {
+                id: "v_auto_1771556274",
+                label: "영웅 화면 15",
+                transcript: "아, 사석에서는 그냥 이름으로 부를 때도 많죠. 너무 격식을 차리면 거리감이 느껴질 테니까요. 사실 저도 그냥 이름으로 불리는 쪽이 더 좋긴 합니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 15.mp3"
+            },
+            {
+                id: "v_auto_1771556308",
+                label: "영웅 화면 16",
+                transcript: "린과 샬롯을 보고 있으면, 서로 참 긍정적인 영향을 끼치는 것 같아서 마음이 흐뭇해지네요. 마음 편한 친구의 존재는 중요하군요.",
+                audio: "johan/light_johan/빛요한 영웅 화면 16.mp3"
+            },
+            {
+                id: "v_auto_1771556338",
+                label: "영웅 화면 17",
+                transcript: "로드를 만나기 전에는 지금과는 많이 달랐었죠. 생각도, 마음가짐도. 감사합니다. 로드.",
+                audio: "johan/light_johan/빛요한 영웅 화면 17.mp3"
+            },
+            {
+                id: "v_auto_1771556378",
+                label: "영웅 화면 18",
+                transcript: "누군가를 이해하려면, 그저 보기만 해서는 힘들 거라 생각합니다. 다가가고, 대화를 나누고, 마음을 알아야 가능한 일이겠죠.",
+                audio: "johan/light_johan/빛요한 영웅 화면 18.mp3"
+            },
+            {
+                id: "v_auto_1771556469",
+                label: "영웅 화면 19",
+                transcript: "예? 아, 아, 하하! 글쎄요. 저도 모든 사람을 좋아할 수는…. 없…겠죠. 로드도 아시지 않습니까? 제 성격.",
+                audio: "johan/light_johan/빛요한 영웅 화면 19.mp3"
+            },
+            {
+                id: "v_auto_1771556515",
+                label: "영웅 화면 20",
+                transcript: "오직 로드만이, 제게 손을 내밀어 주셨습니다. 그때부터였죠. 제 세상이 바뀌었던 건.",
+                audio: "johan/light_johan/빛요한 영웅 화면 20.mp3"
+            },
+                        {
+                id: "v_auto_1771561688",
+                label: "영웅 화면 21",
+                transcript: "만약, 제가 섬기는 분이 로드가 아니라면요? 음…. 그런 생각은 해 본 적이 없는데요.",
+                audio: "johan/light_johan/빛요한 영웅 화면 21.mp3"
+            },
+            {
+                id: "v_auto_1771561715",
+                label: "영웅 화면 22",
+                transcript: "자이라 경에게는 배울 점이 정말 많습니다. 단순히 용맹하기만 한 분이 아니죠. 자이라 경의 가장 큰 장점은 판단력이라고 생각합니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 22.mp3"
+            },
+            {
+                id: "v_auto_1771561769",
+                label: "영웅 화면 23",
+                transcript: "하하하! 크롬경과의 대련은 정말 즐겁습니다. 그만한 실력을 가진 분은 찾기 어려우니까요. '이런 게 정통 검술이구나' 하면서, 항상 감탄합니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 23.mp3"
+            },
+            {
+                id: "v_auto_1771561814",
+                label: "영웅 화면 24",
+                transcript: "어…. 네. 아무래도 눈치는, 그럭저럭 있는 것 같습니다. 그게 없었으면 어릴 때 살아남기 힘들었겠죠.",
+                audio: "johan/light_johan/빛요한 영웅 화면 24.mp3"
+            },
+            {
+                id: "v_auto_1771561873",
+                label: "영웅 화면 25",
+                transcript: "가끔 예전 빈민가 터를 가보긴 하는데 항상 여러 가지 생각이 듭니다. 지금은 사라진 자리에서, 얼마나 많은 사람들이 살아가고 있었는지. …하아, 다들 힘들었을 거예요.",
+                audio: "johan/light_johan/빛요한 영웅 화면 25.mp3"
+            },
+            {
+                id: "v_auto_1771561924",
+                label: "영웅 화면 26",
+                transcript: "로드, 안경이…. 그렇게, 중요할까요?",
+                audio: "johan/light_johan/빛요한 영웅 화면 26.mp3"
+            },
+            {
+                id: "v_auto_1771562002",
+                label: "영웅 화면 27",
+                transcript: "우리는. 사람을 지키는 방패요, 적을 베는 검이라. 그대들의 충성은 세상을 향하고, 그대들의 마음은, 사람을 향하라.",
+                audio: "johan/light_johan/빛요한 영웅 화면 27.mp3"
+            },
+            {
+                id: "v_auto_1771562032",
+                label: "영웅 화면 28",
+                transcript: "기사는 언제나 사람들의 모범이 되어야 하죠. 힘에 취해 버리면 그저 도적이 될 뿐입니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 28.mp3"
+            },
+            {
+                id: "v_auto_1771562076",
+                label: "영웅 화면 29",
+                transcript: "의무와 책임을 다하라는 로드의 말씀, 항상 가슴에 새기고 있습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 29.mp3"
+            },
+            {
+                id: "v_auto_1771562128",
+                label: "영웅 화면 30",
+                transcript: "사람은 변할 수 있다고 하셨던 말씀이, 제게는 희망이었습니다. 그리고 이제는, 다른 뜻으로 다가오기도 하네요. 변하지 않도록 항상 경계하겠습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 30.mp3"
+            },
+            {
+                id: "v_auto_1771562171",
+                label: "영웅 화면 31",
+                transcript: "오스칼 님의 관리 능력에는 항상 감탄만 나오네요. 어딜 가도 먼지 한 톨 찾아볼 수가 없어요.",
+                audio: "johan/light_johan/빛요한 영웅 화면 31.mp3"
+            },
+            {
+                id: "v_auto_1771562233",
+                label: "영웅 화면 32",
+                transcript: "항상 전략 전술의 조언을 구할 분이 로드 뿐이라 부담을 드리는 것 같았는데, 이제 다른 분들이 많이 계셔서 기쁘네요. 특히 크롬 경과 올가 경께는 정말 많이 배우고 있습니다.",
+                audio: "johan/light_johan/빛요한 영웅 화면 32.mp3"
+            }
+      
         ]
     },
         {
